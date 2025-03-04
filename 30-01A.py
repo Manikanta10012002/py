78ba1f82cdaa40e9de5cba1bfe271423d4f69e82
Manikanta10012002 <126399704+Manikanta10012002@users.noreply.github.com>
2025-03-05 01:04:27 +0530

    Add files via upload

---
+++ 30-01A.py
@@ -0,0 +1,19 @@
+#  Write a Python program that checks if a number is positive. If it is, print "The number is positive".
+num = int(input("Enter the number: "))
+if num>0:
+    print("The number is postive")
+# Write a Python program that checks if a number is even or odd
+num1 = int(input("Enter the number"))
+if num1 % 2==0:
+    print("Given number is Even") 
+else:
+    print("Given number is Odd")    
+
+# Write a Python program that checks if a number is positive, negative, or zero.    
+num2 = int(input("Enter the number: "))
+if num2>0:
+    print("The Given number is positive ")
+elif num2==0:
+    print("The Given number is Zero")  
+else:
+    print("The Given number is Negative")      
